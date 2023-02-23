3af7694d5b85b49d2f55ed4a30ba7ceaa2128a15
abigailmgarcia <120236214+abigailmgarcia@users.noreply.github.com>
2023-02-23 17:06:13 -0600

    linked script JS since moved to different folder

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // Assignment code here
-
+function generatePassword() {
+  console.log("hey you clicked the button!")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
