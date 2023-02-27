d8d49c5fdc1a68bbb4fa76da22cae20d5e591567
abigailmgarcia <120236214+abigailmgarcia@users.noreply.github.com>
2023-02-27 00:05:05 -0600

    added if else statement

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 // Assignment code here
+// function generatePassword and it's variables(password criteria) we will be using throughout code
 function generatePassword() {
   var passwordLength;
   var includeLowercase;
-  var inlcudeUppercase;
+  var includeUppercase;
   var inlcudeNumericval;
   var includeSpecialchar;
+  console.log("you clicked button")
 
+// password length between 8-128
+  passwordLength = prompt("Enter a password length between 8 and 128 characters");
+  console.log(passwordLength);
+
+//lowercase, upppercase, special characters
+includeLowercase = confirm("Include lowercase characters?");
+includeUppercase = confirm("Include uppercase characters?");
+inlcudeNumericval = confirm("Include numeric values?");
+includeSpecialchar = confirm("Include special characters?");
+
+// need to validate input 
+if(!includeLowercase && !includeUppercase && !inlcudeNumericval && !includeSpecialchar) {
+  alert("Invalid. Must select at least one character type.");
+} else { 
+  console.log(generatePassword);
+}
 
   return "generated password will go here";
 }
@@ -27,15 +45,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//prompt the user for the password criteriaa
-// password length between 8-128
-  passwordLength = prompt("Enter a password length between 8 and 128 characters");
-  console.log(passwordLength);
-//lowercase, upppercase, special characters
-  includeLowercase = confirm("Include lowercase characters?");
-  includeUppercase = confirm("Include uppercase characters?");
-  includeNumeric = confirm("Include numeric values?");
-  includeSpecial = confirm("Include special characters?");
-// need to validate input 
+
+
 // generate password
 // display password to page (by return?)
