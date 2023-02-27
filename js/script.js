0708a90e309147d7007d8740d722e4c941780095
abigailmgarcia <120236214+abigailmgarcia@users.noreply.github.com>
2023-02-26 23:20:29 -0600

    added prompts to code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // Assignment code here
 function generatePassword() {
   console.log("hey you clicked the button!")
+
+
+  return "generated password will go here";
 }
 
 // Get references to the #generate element
@@ -18,3 +21,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//prompt the user for the password criteriaa
+// password length between 8-128
+  passwordLength = prompt("Enter a password length between 8 and 128 characters");
+  console.log(passwordLength);
+//lowercase, upppercase, special characters
+  includeLowercase = confirm("Include lowercase characters?");
+  includeUppercase = confirm("Include uppercase characters?");
+  includeNumeric = confirm("Include numeric values?");
+  includeSpecial = confirm("Include special characters?");
+// need to validate input 
+// generate password
+// display password to page (by return?)
