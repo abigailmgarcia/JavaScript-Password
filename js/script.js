4c4d1570b7c0fd05e0b8e58d41329704a5806bca
abigailmgarcia <abigailmg31@gmail.com>
2023-06-12 22:21:28 -0500

    debugged numericVal problem

--- js/script.js
+++ js/script.js
@@ -9,23 +9,46 @@ function generatePassword() {
   console.log("you clicked button")
 
 // password length between 8-128
+
   passwordLength = prompt("Enter a password length between 8 and 128 characters");
   console.log(passwordLength);
 
 //lowercase, upppercase, special characters
 includeLowercase = confirm("Include lowercase characters?");
 includeUppercase = confirm("Include uppercase characters?");
-inlcudeNumericval = confirm("Include numeric values?");
+includeNumericval = confirm("Include numeric values?");
 includeSpecialchar = confirm("Include special characters?");
 
-// need to validate input 
-if(!includeLowercase && !includeUppercase && !inlcudeNumericval && !includeSpecialchar) {
-  alert("Invalid. Must select at least one character type.");
-} else { 
-  console.log(generatePassword);
+//variables for characters
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericValues = "0123456789";
+var specialCharacters= "!@#$%^&*()_-=+~`?,.;";
+
+var charSet = "";
+if (includeLowercase) {
+  charSet += lowercaseCharacters;
+}
+if (includeUppercase) {
+  charSet += uppercaseCharacters;
+}
+if (includeNumericval) {
+  charSet += numericValues;
+}
+if (includeSpecialchar) {
+  charSet += specialCharacters;
 }
 
-  return "generated password will go here";
+// need to validate input 
+if(!includeLowercase && !includeUppercase && !includeNumericval && !includeSpecialchar) {
+  alert("Invalid. Must select at least one character type.");
+} else { 
+
+}
+console.log()
+
+
+  return(writePassword); //"generated password will go here";
 }
 
 // Get references to the #generate element
